6b0531221c3a68d5585e52682eda96e76d26668b
--global <--global>
2022-07-16 14:49:20 +0300

    feat: add slider functional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,71 @@
 window.addEventListener('DOMContentLoaded', () => {
-/*     if (window.innerWidth < 1024) { */
-        document.getElementById('headerBurgerBtn').addEventListener('click', function () {//burger-кнопка открытия меню навигации
-            toggleElementScroll('.site-wrapper');
-            openElement('#navigation');
-            openElement('.shadow-box');
-        });
-            
-        document.getElementById('navigationCloseBtn').addEventListener('click', function () {//close-кнопка закрытия меню навигации
-            toggleElementScroll('.site-wrapper');
-            closeElement('#navigation');
-            closeElement('.shadow-box');
-        });
-    
-        document.querySelector('.shadow-box').addEventListener('click', function () {//Фон-затемнение закрывающий меню навигации
-            toggleElementScroll('.site-wrapper');
-            closeElement('#navigation');
-            closeElement('.shadow-box');
-        });
-    
-        document.querySelector('.close-navigation').addEventListener('click', function (event) {//Ведущиая в никуда ссылка в меню навигации закрывающая меню навигации
-            event.preventDefault();
-            closeElement('#navigation');
-            closeElement('.shadow-box');
-        });
+    /*     if (window.innerWidth < 1024) { */
+    document.getElementById('headerBurgerBtn').addEventListener('click', function () {//burger-кнопка открытия меню навигации
+        toggleElementScroll('.site-wrapper');
+        openElement('#navigation');
+        openElement('.shadow-box');
+    });
 
-        document.querySelector('#navigation')?.addEventListener('click', function (event) {//Навигация, где отслеживается клик по ссылке для закрытия самой навигации
-            const target = event.target
-    
-            if (target.classList.contains('navigation__list-link')) {
-                toggleElementScroll('.site-wrapper');
-                closeElement('.shadow-box');
-                closeElement('#navigation');
+    document.getElementById('navigationCloseBtn').addEventListener('click', function () {//close-кнопка закрытия меню навигации
+        toggleElementScroll('.site-wrapper');
+        closeElement('#navigation');
+        closeElement('.shadow-box');
+    });
 
-            }
-        });
-/*     }; */
+    document.querySelector('.shadow-box').addEventListener('click', function () {//Фон-затемнение закрывающий меню навигации
+        toggleElementScroll('.site-wrapper');
+        closeElement('#navigation');
+        closeElement('.shadow-box');
+    });
+
+    document.querySelector('.close-navigation').addEventListener('click', function (event) {//Ведущиая в никуда ссылка в меню навигации закрывающая меню навигации
+        event.preventDefault();
+        closeElement('#navigation');
+        closeElement('.shadow-box');
+    });
+
+    document.querySelector('#navigation')?.addEventListener('click', function (event) {//Навигация, где отслеживается клик по ссылке для закрытия самой навигации
+        const target = event.target
+
+        if (target.classList.contains('navigation__list-link')) {
+            toggleElementScroll('.site-wrapper');
+            closeElement('.shadow-box');
+            closeElement('#navigation');
+
+        }
+    });
+
+    const swiper = new Swiper('.destinations-slider .swiper-container', {
+/*         loop: true, */
+        slidesPerView: 1,
+        spaceBetween: 38,
+        slideToClickedSlide: true,
+        speed: 1500,
+        /*         autoplay: {
+                    delay: 3000,
+                }, */
+
+        watchSlidesProgress: true,
+
+        // pagination
+        pagination: {
+            el: '.destinations-slider .destinations-slider__tabs',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.destinations-slider .destinations-slider__nav-button--next',
+            prevEl: '.destinations-slider .destinations-slider__nav-button--prev',
+        }
+
+    });
+    /*     }; */
 
     console.log(
-        "Вёрстка соответствует макету. Ширина экрана 390px - выполнено (+48)\nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется - выполнено (+15)\nНа ширине экрана 390рх и меньше реализовано адаптивное меню - выполнено (+22)\nИтого 85 баллов\nХОРОШЕГО ДНЯ:)"
+        ""
     );
 });
+
 
 function toggleElementScroll(element) {
     document.querySelector(element).classList.toggle("_false-scroll");
